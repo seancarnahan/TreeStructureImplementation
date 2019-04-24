c4e4764ad8cd64b96aa49d962c1c2007e5dd21e7
seancarnahan <Vhellendoorn@ucdavis.edu>
2019-04-23 19:08:53 -0700

    4/23/19: safe commit to default status

--- Makefile
+++ Makefile
@@ -17,13 +17,13 @@ TESTBIN_DIR = ./testbin
 
 UNAME := $(shell uname)
 
-PKGS = 
+PKGS =
 
 #DEBUG_MODE=TRUE
 ifdef DEBUG_MODE
 DEFINES  += -DDEBUG
 CFLAGS   += -g -ggdb -D_GLIBCXX_DEBUG
-else 
+else
 CFLAGS   += -O3
 endif
 
@@ -35,9 +35,9 @@ endif
 
 INCLUDE         += -I $(INC_DIR)
 CFLAGS          +=  -Wall
-LDFLAGS         += 
+LDFLAGS         +=
 TESTLDFLAGS     += -lgtest -lgtest_main  -lpthread
-CPPFLAGS        += -std=c++14 
+CPPFLAGS        += -std=c++14
 PROJ_NAME       = proj2
 TESTSTR_NAME    = teststr
 TESTPATH_NAME   = testpath
@@ -49,30 +49,30 @@ TESTTREE_OBJ    = $(OBJ_DIR)/testtree.o
 
 PROJ_OBJS       = $(OBJ_DIR)/DirectoryListing.o     \
                   $(OBJ_DIR)/FileSystemTree.o
-      
-            
-            
+
+
+
 all: directories test $(BIN_DIR)/$(PROJ_NAME)
 test: $(TESTBIN_DIR)/$(TESTTREE_NAME)
 	$(TESTBIN_DIR)/$(TESTTREE_NAME)
-	
-$(BIN_DIR)/$(PROJ_NAME): $(PROJ_OBJS) $(MAIN_OBJ)
-	$(CXX) $(MAIN_OBJ) $(PROJ_OBJS) -o $(BIN_DIR)/$(PROJ_NAME) $(CFLAGS) $(CPPFLAGS) $(DEFINES) $(LDFLAGS) 
-	
-$(TESTBIN_DIR)/$(TESTTREE_NAME): $(PROJ_OBJS) $(TESTTREE_OBJ)
-	$(CXX) $(PROJ_OBJS) $(TESTTREE_OBJ) -o $(TESTBIN_DIR)/$(TESTTREE_NAME) $(CFLAGS) $(CPPFLAGS) $(DEFINES) $(TESTLDFLAGS) 
 
-	
+$(BIN_DIR)/$(PROJ_NAME): $(PROJ_OBJS) $(MAIN_OBJ)
+	$(CXX) $(MAIN_OBJ) $(PROJ_OBJS) -o $(BIN_DIR)/$(PROJ_NAME) $(CFLAGS) $(CPPFLAGS) $(DEFINES) -L ./googletest/build/lib $(LDFLAGS)
+
+$(TESTBIN_DIR)/$(TESTTREE_NAME): $(PROJ_OBJS) $(TESTTREE_OBJ)
+	$(CXX) $(PROJ_OBJS) $(TESTTREE_OBJ) -o $(TESTBIN_DIR)/$(TESTTREE_NAME) $(CFLAGS) $(CPPFLAGS) $(DEFINES) -L ./googletest/build/lib $(TESTLDFLAGS)
+
+
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp
-	$(CXX) $(CFLAGS) $(CPPFLAGS) $(DEFINES) $(INCLUDE) -c $< -o $@
-	
+	$(CXX) $(CFLAGS) $(CPPFLAGS) $(DEFINES) $(INCLUDE) -I ./googletest/googletest/include -c $< -o $@
+
 .PHONY: directories
 directories:
 	mkdir -p $(BIN_DIR)
 	mkdir -p $(OBJ_DIR)
 	mkdir -p $(TESTBIN_DIR)
-	
+
 clean::
 	-rm $(OBJ_DIR)/*.o $(INC_DIR)/*.*~ $(SRC_DIR)/*.*~
-	
+
 .PHONY: clean
--- src/FileSystemTree.cpp
+++ src/FileSystemTree.cpp
@@ -1,11 +1,15 @@
 #include "FileSystemTree.h"
-
+#include <gtest/gtest.h>
+//structures
 struct CFileSystemTree::SImplementation{
-    // You implementation here
+    std::string obj = "/";
+
 };
 
 struct CFileSystemTree::CEntry::SImplementation{
-    // You implementation here
+    //std::weak_ptr parentPoint= nullptr;
+    //std::vector<int> v;
+
 };
 
 struct CFileSystemTree::CEntryIterator::SImplementation{
@@ -16,6 +20,9 @@ struct CFileSystemTree::CConstEntryIterator::SImplementation{
     // You implementation here
 };
 
+
+
+//constructors
 CFileSystemTree::CEntry::CEntry() : DImplementation(std::make_unique< SImplementation >()){
     // You code here
 }
@@ -28,96 +35,135 @@ CFileSystemTree::CEntry::~CEntry(){
     // You code here
 }
 
+
+//this is the pointer to the current instance
 CFileSystemTree::CEntry &CFileSystemTree::CEntry::operator=(const CEntry &entry){
-    // You code here
+    if (this != &entry) {
+      //assign things here
+    }
+    return *this;
 }
+
+
+
 
 bool CFileSystemTree::CEntry::Valid() const{
     // You code here
+    return true;
 }
 
 std::string CFileSystemTree::CEntry::Name() const{
     // You code here
+    return "";
 }
 
 std::string CFileSystemTree::CEntry::FullPath() const{
     // You code here
+    return "";
 }
 
+//this method and the below method are going to do the same thing
+//the below operator just calls this method
 std::string CFileSystemTree::CEntry::ToString() const{
-    // You code here
+  return "";
 }
 
+
+
+
+
+
+//not overloading the operator -> instead you are overloading to string
 CFileSystemTree::CEntry::operator std::string() const{
     // You code here
+    return this->ToString();
 }
 
 bool CFileSystemTree::CEntry::Rename(const std::string &name){
     // You code here
+    return true;
 }
 
 size_t CFileSystemTree::CEntry::ChildCount() const{
     // You code here
+    size_t foo = 1;
+    return foo;
 }
 
 bool CFileSystemTree::CEntry::SetChild(const std::string &name, CEntryIterator &iter){
     // You code here
+    return true;
 }
 
 bool CFileSystemTree::CEntry::AddChild(const std::string &path, bool addall){
     // You code here
+    return true;
 }
 
 bool CFileSystemTree::CEntry::RemoveChild(const std::string &path){
     // You code here
+    return true;
 }
 
 bool CFileSystemTree::CEntry::SetData(const std::vector< char > &data){
     // You code here
+    return true;
 }
 
 bool CFileSystemTree::CEntry::GetData(std::vector< char > &data) const{
     // You code here
+    return true;
 }
 
+
 CFileSystemTree::CEntry &CFileSystemTree::CEntry::Parent(){
-    // You code here
+    return *this;
+
 }
 
 const CFileSystemTree::CEntry &CFileSystemTree::CEntry::Parent() const{
-    // You code here
+    return *this;
 }
 
 CFileSystemTree::CEntryIterator CFileSystemTree::CEntry::Find(const std::string &name){
     // You code here
+    std::unique_ptr<CFileSystemTree::CEntryIterator> iterPtr = std::make_unique<CFileSystemTree::CEntryIterator>();
+    return *iterPtr;
 }
 
 CFileSystemTree::CConstEntryIterator CFileSystemTree::CEntry::Find(const std::string &name) const{
-    // You code here
+  std::unique_ptr<CFileSystemTree::CConstEntryIterator> iterPtr = std::make_unique<CFileSystemTree::CConstEntryIterator>();
+  return *iterPtr;
 }
 
 CFileSystemTree::CEntryIterator CFileSystemTree::CEntry::begin(){
-    // You code here
+  std::unique_ptr<CFileSystemTree::CEntryIterator> iterPtr = std::make_unique<CFileSystemTree::CEntryIterator>();
+  return *iterPtr;
 }
 
 CFileSystemTree::CConstEntryIterator CFileSystemTree::CEntry::begin() const{
-    // You code here
+  std::unique_ptr<CFileSystemTree::CConstEntryIterator> iterPtr = std::make_unique<CFileSystemTree::CConstEntryIterator>();
+  return *iterPtr;
 }
 
 CFileSystemTree::CConstEntryIterator CFileSystemTree::CEntry::cbegin() const{
-    // You code here
+  std::unique_ptr<CFileSystemTree::CConstEntryIterator> iterPtr = std::make_unique<CFileSystemTree::CConstEntryIterator>();
+  return *iterPtr;
 }
 
 CFileSystemTree::CEntryIterator CFileSystemTree::CEntry::end(){
-    // You code here
+  std::unique_ptr<CFileSystemTree::CEntryIterator> iterPtr = std::make_unique<CFileSystemTree::CEntryIterator>();
+  return *iterPtr;
 }
 
 CFileSystemTree::CConstEntryIterator CFileSystemTree::CEntry::end() const{
-    // You code here
+  std::unique_ptr<CFileSystemTree::CConstEntryIterator> iterPtr = std::make_unique<CFileSystemTree::CConstEntryIterator>();
+  return *iterPtr;
 }
 
 CFileSystemTree::CConstEntryIterator CFileSystemTree::CEntry::cend() const{
-    // You code here
+  std::unique_ptr<CFileSystemTree::CConstEntryIterator> iterPtr = std::make_unique<CFileSystemTree::CConstEntryIterator>();
+  return *iterPtr;
 }
 
 CFileSystemTree::CEntryIterator::CEntryIterator() : DImplementation(std::make_unique< SImplementation >()){
@@ -133,41 +179,44 @@ CFileSystemTree::CEntryIterator::~CEntryIterator(){
 }
 
 CFileSystemTree::CEntryIterator& CFileSystemTree::CEntryIterator::operator=(const CEntryIterator  &iter){
-    // You code here
+    return *this;
 }
 
 bool CFileSystemTree::CEntryIterator::operator==(const CEntryIterator &iter) const{
-    // You code here
+    return true;
 }
 
 bool CFileSystemTree::CEntryIterator::operator!=(const CEntryIterator &iter) const{
-    // You code here
+    return true;
 }
 
 CFileSystemTree::CEntryIterator& CFileSystemTree::CEntryIterator::operator++(){
-    // You code here
+    return *this;
 }
 
 CFileSystemTree::CEntryIterator CFileSystemTree::CEntryIterator::operator++(int){
-    // You code here
+    return *this;
 }
 
 CFileSystemTree::CEntryIterator& CFileSystemTree::CEntryIterator::operator--(){
-    // You code here
+    return *this;
 }
 
 CFileSystemTree::CEntryIterator CFileSystemTree::CEntryIterator::operator--(int){
-    // You code here
+    return *this;
 }
 
 CFileSystemTree::CEntry &CFileSystemTree::CEntryIterator::operator*() const{
-    // You code here
+  std::unique_ptr<CFileSystemTree::CEntry> iterPtr = std::make_unique<CFileSystemTree::CEntry>();
+  return *iterPtr;
 }
 
 CFileSystemTree::CEntry *CFileSystemTree::CEntryIterator::operator->() const{
-    // You code here
+  CFileSystemTree::CEntry *foo = new CFileSystemTree::CEntry();
+  return foo;
 }
 
+//constructors for CConstEntryIterator
 CFileSystemTree::CConstEntryIterator::CConstEntryIterator() : DImplementation(std::make_unique< SImplementation >()){
 
 }
@@ -184,42 +233,47 @@ CFileSystemTree::CConstEntryIterator::~CConstEntryIterator(){
     // You code here
 }
 
+
+
 CFileSystemTree::CConstEntryIterator& CFileSystemTree::CConstEntryIterator::operator=(const CConstEntryIterator &iter){
-    // You code here
+    return *this;
 }
 
 bool CFileSystemTree::CConstEntryIterator::operator==(const CConstEntryIterator &iter) const{
-    // You code here
+    return true;
 }
 
 bool CFileSystemTree::CConstEntryIterator::operator!=(const CConstEntryIterator &iter) const{
-    // You code here
+    return true;
 }
 
 CFileSystemTree::CConstEntryIterator& CFileSystemTree::CConstEntryIterator::operator++(){
-    // You code here
+    return *this;
 }
 
 CFileSystemTree::CConstEntryIterator CFileSystemTree::CConstEntryIterator::operator++(int){
-    // You code here
+    return *this;
 }
 
 CFileSystemTree::CConstEntryIterator& CFileSystemTree::CConstEntryIterator::operator--(){
-    // You code here
+    return *this;
 }
 
 CFileSystemTree::CConstEntryIterator CFileSystemTree::CConstEntryIterator::operator--(int){
-    // You code here
+    return *this;
 }
 
 const CFileSystemTree::CEntry &CFileSystemTree::CConstEntryIterator::operator*() const{
-    // You code here
+  std::unique_ptr<CFileSystemTree::CEntry> iterPtr = std::make_unique<CFileSystemTree::CEntry>();
+  return *iterPtr;
 }
 
 const CFileSystemTree::CEntry *CFileSystemTree::CConstEntryIterator::operator->() const{
-    // You code here
+  CFileSystemTree::CEntry *foo = new CFileSystemTree::CEntry();
+  return foo;
 }
 
+//constructors for CFileSystemTree
 CFileSystemTree::CFileSystemTree() : DImplementation(std::make_unique< SImplementation >()){
     // You code here
 }
@@ -232,38 +286,46 @@ CFileSystemTree::~CFileSystemTree(){
     // You code here
 }
 
+
 CFileSystemTree &CFileSystemTree::operator=(const CFileSystemTree &tree){
-    // You code here
+    return *this;
 }
 
 CFileSystemTree::CEntry &CFileSystemTree::Root(){
-    // You code here
+  std::unique_ptr<CFileSystemTree::CEntry> iterPtr = std::make_unique<CFileSystemTree::CEntry>();
+  
+  return *iterPtr;
 }
 
 const CFileSystemTree::CEntry &CFileSystemTree::Root() const{
-    // You code here
+  std::unique_ptr<CFileSystemTree::CEntry> iterPtr = std::make_unique<CFileSystemTree::CEntry>();
+  return *iterPtr;
 }
 
 std::string CFileSystemTree::ToString() const{
-    // You code here
+    return "";
 }
 
 CFileSystemTree::operator std::string() const{
-    // You code here
+    return this->ToString();
 }
 
 CFileSystemTree::CEntryIterator CFileSystemTree::Find(const std::string &path){
-    // You code here
+  std::unique_ptr<CFileSystemTree::CEntryIterator> iterPtr = std::make_unique<CFileSystemTree::CEntryIterator>();
+  return *iterPtr;
 }
 
 CFileSystemTree::CConstEntryIterator CFileSystemTree::Find(const std::string &path) const{
-    // You code here
+  std::unique_ptr<CFileSystemTree::CConstEntryIterator> iterPtr = std::make_unique<CFileSystemTree::CConstEntryIterator>();
+  return *iterPtr;
 }
 
 CFileSystemTree::CEntryIterator CFileSystemTree::NotFound(){
-    // You code here
+  std::unique_ptr<CFileSystemTree::CEntryIterator> iterPtr = std::make_unique<CFileSystemTree::CEntryIterator>();
+  return *iterPtr;
 }
 
 CFileSystemTree::CConstEntryIterator CFileSystemTree::NotFound() const{
-    // You code here
+  std::unique_ptr<CFileSystemTree::CConstEntryIterator> iterPtr = std::make_unique<CFileSystemTree::CConstEntryIterator>();
+  return *iterPtr;
 }
